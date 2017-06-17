29d5ba320a1f584151298de35b2b32b230cf6403
lq150415 <lq150415@gmail.com>
2017-06-17 10:05:55 -0400

    daynor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">BIENVENIDO DAYNOR HERRERA</div>
+                <div class="title">BIENVENIDO DAYNOR Hjdsk</div>
 
             </div>
         </div>
